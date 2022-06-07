20248fc1afb28b57339aeccf9a512c3d8829e1ab
Arjo Chakravarty <arjo@openrobotics.org>
2022-06-07 17:48:13 +0800

    Generify
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Crowd simulation framework
+
+> :warning: Under construction
+
+The following is going to be a crowd simulation framework written in rust.
+
+It currently is just me experimenting with stuff.
--- rmf_crowdsim/src/lib.rs
+++ rmf_crowdsim/src/lib.rs
@@ -28,7 +28,7 @@ pub trait Map {
 }
 
 /// Abstract interface for planners
-pub trait HighLevelPlanner {
+pub trait HighLevelPlanner<M :Map> {
 
     /// Gets the desired velocity given an agent. If the plan has completed return None
     fn get_desired_velocity(&mut self, agent: &Agent, time: std::time::Duration) -> Option<Vec2f>;
@@ -40,24 +40,24 @@ pub trait HighLevelPlanner {
     fn remove_agent_id(&mut self, agent: AgentId);
 
 
-    fn set_map(&mut self, map: Arc<dyn Map>);
+    fn set_map(&mut self, map: Arc<M>);
 }
 
-pub trait LocalPlanner {
-    fn get_desired_velocity(&self, agent: &Agent, recommended_velocity: Vec2f, map: Arc<dyn Map>) -> Vec2f;
+pub trait LocalPlanner<M: Map> {
+    fn get_desired_velocity(&self, agent: &Agent, recommended_velocity: Vec2f, map: Arc<M>) -> Vec2f;
 }
 
-pub struct Simulation {
+pub struct Simulation<M: Map, H: HighLevelPlanner<M>, L: LocalPlanner<M>> {
     agents: Vec<Agent>,
-    map: Arc<dyn Map>,
-    high_level_planner: HashMap<AgentId, Arc<Mutex<dyn HighLevelPlanner>>>,
-    local_planner: HashMap<AgentId, Arc<dyn LocalPlanner>>,
+    map: Arc<M>,
+    high_level_planner: HashMap<AgentId, Arc<Mutex<H>>>,
+    local_planner: HashMap<AgentId, Arc<L>>,
     sim_time: std::time::Duration
 }
 
-impl Simulation {
+impl<M: Map, H: HighLevelPlanner<M>, L: LocalPlanner<M>> Simulation<M,H,L> {
 
-    fn new(map: Arc<dyn Map>) -> Self
+    fn new(map: Arc<M>) -> Self
     {
         Self {
             agents: vec!(),
@@ -71,8 +71,8 @@ impl Simulation {
     fn add_agents(
         &mut self,
         spawn_positions: &Vec<Point>,
-        high_level_planner: Arc<Mutex<dyn HighLevelPlanner>>,
-        local_planner: Arc<dyn LocalPlanner>) -> Vec<AgentId>
+        high_level_planner: Arc<Mutex<H>>,
+        local_planner: Arc<L>) -> Vec<AgentId>
     {
         let mut res = Vec::<AgentId>::new();
         for x in spawn_positions
